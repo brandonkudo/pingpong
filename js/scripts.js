703ef653755b7f12c0d4626dc4ca13574fc540e3
Brandon Kudo <brandonkudo@gmail.com>
2017-09-22 15:16:40 -0700

    add color to jumbotron, add well, change font in jumbotron

--- js/scripts.js
+++ js/scripts.js
@@ -1,36 +1,37 @@
 var pingPong = function(userInput) {
-  var numberArray = [];
-  if (userInput > 0) { //positive numbers
+  var arrayNumbers = [];
+  if (userInput > 0) {
       for (var i = 1; i <= userInput; i++) {
-        numberArray.push(i);
+        arrayNumbers.push(i);
       }
-      numberArray.forEach(function(number) {
+      arrayNumbers.forEach(function(number) {
         if (number % 15 === 0) {
-          numberArray[number-1] = 'pingpong';
+          arrayNumbers[number-1] = 'ping-pong';
         } else if (number % 3 === 0) {
-          numberArray[number-1] = 'ping';
+          arrayNumbers[number-1] = 'ping';
         } else if (number % 5 === 0) {
-          numberArray[number-1] = 'pong';
+          arrayNumbers[number-1] = 'pong';
         }
       });
   }
-  return numberArray;
+  return arrayNumbers;
 }
 
-var showResults = function(outputArray) {
-    $('ul#result').empty();
-  outputArray.forEach(function(item) {
-    $('ul#result').append('<li>' + item + '</li>');
-  });
-  return;
-}
+
 
 $(document).ready(function() {
   $('form#formOne').submit(function(event) {
+    event.preventDefault();
     var userInput = $('input#userInput').val();
+    var showResults = function(arrayOutput) {
+        $('ul#result').empty();
+      arrayOutput.forEach(function(item) {
+        $('ul#result').append('<li>' + item + '</li>');
+      });
+      return;
+    }
 
     showResults(pingPong(userInput));
 
-    event.preventDefault();
   });
 });
